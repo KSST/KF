1d89ce98489620d7f28241cac9c262c08a7b6707
jakoch <jakoch@web.de>
2012-10-07 16:09:28 +0200

    throw runtimeexception if mapfile not set

--- framework/Koch/Autoload/Loader.php
+++ framework/Koch/Autoload/Loader.php
@@ -331,6 +331,10 @@ class Loader
      */
     public static function readAutoloadingMapFile()
     {
+        if(self::$mapfile === '') {
+            throw new RuntimeException('No classmap file set. Use method ->setClassMapFile() to set one.');
+        }
+
         // create file, if not existant
         if (is_file(self::$mapfile) === false) {
             $file_resource = fopen($mapfile, 'a', false);
