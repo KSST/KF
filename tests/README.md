0fe27ee4eab1c6b3297c9bdee31c8f0819d9dd39
jakoch <jakoch@web.de>
2012-10-07 12:03:22 +0200

    added TestSuite bootstrap code

--- tests/README.md
+++ tests/README.md
@@ -1,1 +1,20 @@
+                         __________________________
+                         K O C H  F R A M E W O R K
+
+== Test Status
+
+image::https://secure.travis-ci.org/KSST/KF.png?branch=master[CI Status master]
+
+== Tests Overview
+
+We use Simpletest for running automated tests on the Koch Framework.
+
+For running the complete Testsuite simply call ./tests/runAllTests.php.
+
+If you want to help, we are always looking to improve the Koch Framework code coverage!
+
+== Simpletest Project
+You can find the SimpleTest project located at
+http://sourceforge.net/projects/simpletest/
+https://sourceforge.net/project/showfiles.php?group_id=76550
 Tests
---
+++ tests/runAllTests.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * Clansuite - just an eSports CMS
+ * Jens-André Koch © 2005 - onwards
+ * http://www.clansuite.com/
+ *
+ * This file is part of "Clansuite - just an eSports CMS".
+ *
+ * License: GNU/GPL v2 or any later version, see LICENSE file.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// setup env(error/date/paths)
+require_once 'bootstrap.php';
+
+/**
+ * Setup Simpletest
+ */
+require_once 'simpletest/unit_tester.php';
+require_once 'simpletest/test_case.php';
+require_once 'simpletest/reporter.php';
+require_once 'simpletest/web_tester.php';
+
+// hmm, we need to load this, to get rid of the "No runnable test cases in runAlltest" error
+#require_once 'simpletest/autorun.php';
+
+/**
+ * Setup our Testsuite and Reporter
+ */
+require_once 'reporter.php';
+require_once 'testsuite.php';
+require_once 'unittester.php';
+
+// start CodeCoverage
+if (PERFORM_CODECOVERAGE == true) {
+    require_once 'codecoverage.php';
+    Clansuite_CodeCoverage::start();
+}
+
+// Tests -> instantiate Clansuite Testsuite
+$testsuite = new ClansuiteTestsuite();
+$success = false;
+
+// Tests -> determine, if we are in commandline mode, then output pure text
+if (TextReporter::inCli()) {
+
+    // fetch reporter
+    #require_once 'simpletest/extensions/colortext_reporter.php';
+    require_once 'simpletest/extensions/junit_xml_reporter.php';
+    ob_start();
+    #$reporter = new TextReporter();
+    #$reporter = new ColorTextReporter();
+    $reporter = new JUnitXmlReporter();
+
+    // hand reporter to the testsuite and run it
+    $success = $testsuite->run($reporter);
+
+    // write test results to xml file
+    file_put_contents(__DIR__ . '/test-results.xml', ob_get_contents());
+    ob_end_clean();
+
+    // do not let the tests fail, the fail status is evaluated via xml file
+    $success = true;
+} else {
+    // display nice html report
+    $success = $testsuite->run(new Reporter);
+}
+
+// stop CodeCoverage
+if (PERFORM_CODECOVERAGE == true) {
+    Clansuite_CodeCoverage::stop();
+    Clansuite_CodeCoverage::getReport();
+}
+
+// Tests -> exit with status
+if (false === $success) {
+    // Exit with error code to let the build fail, when the test is unsuccessfull.
+    exit(1);
+}
