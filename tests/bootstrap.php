4c54d8e9feed977ee073648c420127560fb95721
Jens A. Koch <jakoch@web.de>
2012-10-07 12:03:22 +0200

    added TestSuite bootstrap code

---
+++ tests/bootstrap.php
@@ -0,0 +1,132 @@
+<?php
+
+/**
+ * Clansuite - just an eSports CMS
+ * Jens A. Koch © 2005 - onwards
+ * http://www.clansuite.com/
+ *
+ * This file is part of "Clansuite - just an eSports CMS".
+* SPDX-License-Identifier: MIT *
+ *
+ * *
+ * *
+ * *
+ */
+
+/**
+ * Configuration Options for Tests
+ * ===================================================
+ */
+
+// Toggle for CodeCoverage. (It depends on the PHP extensions Xdebug and SQlite.)
+define('PERFORM_CODECOVERAGE', false);
+
+// Toggle, whether to run WebTests or not.
+define('PERFORM_WEBTESTS', false);
+
+// ===================================================
+
+// Error Reporting Level
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', true);
+ini_set('html_errors', false);
+ini_set('log_errors', false);
+
+// Tests take some time and memory
+/**
+ * The test for safe_mode is needed in order to avoid the message:
+ * "Warning: set_time_limit() has been disabled for security reasons".
+ * SAFE_MODE has been DEPRECATED as of PHP 5.3.0 and REMOVED as of PHP 5.4.0.
+ * This check is added to get tests without errors on crappy, outdated hosters.
+ */
+if (ini_get('safe_mode') == false) { set_time_limit(0); }
+ini_set('memory_limit', '256M');
+
+// PHP Version Check
+$REQUIRED_PHP_VERSION = '5.3.2';
+if (version_compare(PHP_VERSION, $REQUIRED_PHP_VERSION, '<=') === true) {
+    exit('Your PHP Version is <b><font color="#FF0000">' . PHP_VERSION . '</font></b>.
+         Clansuite Testsuite requires PHP Version <b><font color="#4CC417">' . $REQUIRED_PHP_VERSION . '</font></b> or newer.');
+}
+unset($REQUIRED_PHP_VERSION);
+
+// well this should be defined in PHP.ini.. fallback, if you are lazy.
+date_default_timezone_set('Europe/Berlin');
+
+$paths = array(
+    // add the TEST SUBJECT dir
+    realpath(dirname(__DIR__) . '/framework'),      // /trunk/framework
+    realpath(dirname(__DIR__) . '/Clansuite'),    // /trunk/Clansuite
+    realpath(dirname(__DIR__)),                     // /trunk
+    // adjust include path to TESTS dir
+    realpath(__DIR__),                  // /trunk/tests
+    realpath(__DIR__ . '/unittests'),   // /trunk/tests/unittests
+);
+#var_dump($paths);
+
+// attach original include paths
+set_include_path(implode($paths, PATH_SEPARATOR) . PATH_SEPARATOR . get_include_path());
+unset($paths);
+
+// needed if, run from CLI
+if (empty($_SERVER['SERVER_NAME'])) {
+    $_SERVER['SERVER_NAME'] = gethostname();
+}
+
+//  acquire clansuite path constants
+include dirname(__DIR__) . '/Clansuite/Application.php';
+\Clansuite\Application::define_ConstantsAndPaths();
+\Clansuite\Application::initialize_Loader();
+
+\Koch\Localization\Utf8::initialize();
+
+/**
+ * Constants
+ *
+ * Constants must be defined, after initialize_paths(),
+ * because of the automatic apc constants cache in
+ * define_ConstantsAndPaths().
+ */
+define('REWRITE_ENGINE_ON', 1);
+define('TESTSUBJECT_DIR', dirname(__DIR__) . '/');
+
+/**
+ * We might need some debug utils,
+ * when we are not in CLI mode.
+ */
+if (isCli() === false) {
+    require_once KOCH_FRAMEWORK . 'Debug/Debug.php';
+}
+
+/**
+ * Gettext Replacement
+ *
+ * @param type $msgid
+ */
+if (!function_exists('_')) {
+    function _($msgid)
+    {
+        return $msgid;
+    }
+}
+
+function isCli()
+{
+    if (php_sapi_name() == 'cli' and empty($_SERVER['REMOTE_ADDR'])) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// put more bootstrapping code here
+
+/**
+ * Netbeans Hint
+ *
+ * Project > Properties > PHPUnit
+ * In the Project Properties Dialog
+ * 1) Activate Checkbox "Use Bootstrap"
+ * 2) Activate Checkbox "Use Bootstrap for Creating New Unit Tests"
+ * 3) Use "Browse" and point to this file
+ */
---
+++ tests/codecoverage.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * Clansuite - just an eSports CMS
+ * Jens-Andr� Koch � 2005 - onwards
+ * http://www.clansuite.com/
+ *
+ * This file is part of "Clansuite - just an eSports CMS".
+* SPDX-License-Identifier: MIT *
+ *
+ * *
+ * *
+ * *
+ */
+
+class Clansuite_CodeCoverage
+{
+    public static $coverage;
+
+    /**
+    * Starts the Code Coverage.
+    */
+    public static function start()
+    {
+        /**
+        * Simpletest Code Coverage depends on xdebug.
+        *
+        * Ensure that the xdebug extension is loaded.
+        */
+        if (false === extension_loaded('xdebug')) {
+            die('Code Coverage needs Xdebug extension. Not loaded!');
+        }
+
+        if (false === function_exists("xdebug_start_code_coverage")) {
+            die('Code Coverage needs the method xdebug_start_code_coverage. Not found!');
+        }
+
+        /**
+        * Simpletest Code Coverage depends on sqlite.
+        *
+        * Ensure that the sqlite extension is loaded.
+        */
+        /*if (false === class_exists('SQLiteDatabase')) {
+            echo 'Code Coverage needs the php extension SQLITE. Not loaded!';
+        }*/
+
+        /**
+        * Setup Simpletest Code Coverage.
+        */
+        require_once 'simpletest/extensions/coverage/coverage.php';
+
+        $coverage = new CodeCoverage();
+        $coverage->log = 'coverage.sqlite';
+        $coverage->root = TESTSUBJECT_DIR;
+        $coverage->includes[] = '.*\.php$';
+        $coverage->excludes[] = 'simpletest';
+        $coverage->excludes[] = 'tests';
+        $coverage->excludes[] = 'libraries';
+        $coverage->excludes[] = 'coverage-report';
+        $coverage->excludes[] = 'sweety';
+        $coverage->excludes[] = './.*.php';
+        $coverage->maxDirectoryDepth = 1;
+        $coverage->resetLog();
+        $coverage->writeSettings();
+
+        /**
+        * Finally: let's start the Code Coverage.
+        */
+        $coverage->startCoverage();
+        #echo 'Code Coverage started...';
+
+        self::$coverage = $coverage;
+    }
+
+    /**
+    * Stops the Code Coverage.
+    */
+    public static function stop()
+    {
+        self::$coverage->writeUntouched();
+        self::$coverage->stopCoverage();
+        #echo 'Code Coverage stopped!';
+    }
+
+    /**
+    * Generates the Code Coverage Report.
+    */
+    public static function getReport()
+    {
+        require_once 'simpletest/extensions/coverage/coverage_reporter.php';
+
+        $handler = new CoverageDataHandler(self::$coverage->log);
+        $report = new CoverageReporter();
+        $report->reportDir = 'coverage-report';
+        $report->title = 'Clansuite Coverage Report';
+        $report->coverage = $handler->read();
+        $report->untouched = $handler->readUntouchedFiles();
+        $report->generate();
+    }
+}
