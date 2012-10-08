1ccaf59e4cea2ed994fe9b38ff352d1ffae3256b
Jens A. Koch <jakoch@web.de>
2012-10-08 19:07:30 +0200

    cs

--- framework/Koch/Datagrid/ColumnRenderer/Image.php
+++ framework/Koch/Datagrid/ColumnRenderer/Image.php
@@ -36,6 +36,6 @@ class Image extends ColumnRenderer implements ColumnRendererInterface
             $image_alt = mb_substr($value, 0, $this->nameWrapLength - 5) . 'Image';
         }
 
-        return $this->_replacePlaceholders($value, Clansuite_HTML::img( $value, array( 'alt'  => $image_alt)));
+        return $this->_replacePlaceholders($value, Clansuite_HTML::img($value, array( 'alt'  => $image_alt)));
     }
 }
