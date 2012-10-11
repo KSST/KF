eee7e9e5456fccc15442869ebff32517afb770d5
jakoch <jakoch@web.de>
2012-10-11 13:58:10 +0200

    moved set_include_path nearer to the path array context

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,29 +1,4 @@
 <?php
-
-/**
- * Clansuite - just an eSports CMS
- * Jens-André Koch © 2005 - onwards
- * http://www.clansuite.com/
- *
- * This file is part of "Clansuite - just an eSports CMS".
- *
- * License: GNU/GPL v2 or any later version, see LICENSE file.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 // Error Reporting Level
 error_reporting(E_ALL | E_STRICT);
 
@@ -36,6 +11,8 @@ $paths = array(
     $test,
     get_include_path()
 );
+// attach original include paths
+set_include_path(implode(PATH_SEPARATOR, $paths));
 
 if (!function_exists('_')) {
     function _($msgid)
@@ -43,9 +20,6 @@ if (!function_exists('_')) {
         return $msgid;
     }
 }
-
-// attach original include paths
-set_include_path(implode(PATH_SEPARATOR, $paths));
 
 include __DIR__ . '/_autoload.php';
 
