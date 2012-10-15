62e6b158cbb7fbe0d7f9ea1c627ea7113f2ee57d
jakoch <jakoch@web.de>
2012-10-15 17:45:39 +0200

    added function_exists checks

--- framework/Koch/Localization/MbstringWrapper.php
+++ framework/Koch/Localization/MbstringWrapper.php
@@ -22,7 +22,6 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  */
-
 /**
  * Koch Framework - Class provides MB_STRING Wrapper Methods.
  *
@@ -49,128 +48,169 @@
  *  mb_strtoupper
  *  mb_substr
  *  mb_substr_count
+ * 
+ *  Note: The function_exists() checks are needed to prevent redeclaration 
+ *        errors, which would occur during linting.
  */
 
 /**
  * mb_convert_encoding
  */
-function mb_convert_encoding($str, $to_encoding, $from_encoding = null)
-{
-    if (null === $from_encoding) {
-        return utf8_convert_encoding($str, $to_encoding);
-    } else {
-        return utf8_convert_encoding($str, $to_encoding, $from_encoding);
+if (!function_exists('mb_convert_encoding')) {
+
+    function mb_convert_encoding($str, $to_encoding, $from_encoding = null)
+    {
+        if (null === $from_encoding) {
+            return utf8_convert_encoding($str, $to_encoding);
+        } else {
+            return utf8_convert_encoding($str, $to_encoding, $from_encoding);
+        }
     }
 }
 
 /**
  * mb_detect_encoding
  */
-function mb_detect_encoding($str)
-{
-    return utf8_detect_encoding($str);
+if (!function_exists('mb_detect_encoding')) {
+
+    function mb_detect_encoding($str)
+    {
+        return utf8_detect_encoding($str);
+    }
 }
 
 /**
  * mb_stripos
  */
-function mb_stripos($haystack, $needle, $offset=null)
-{
-    if (null === $offset) {
-        return stripos($haystack, $needle);
-    } else {
-        return stripos($haystack, $needle, $offset);
+if (!function_exists('mb_stripos')) {
+
+    function mb_stripos($haystack, $needle, $offset = null)
+    {
+        if (null === $offset) {
+            return stripos($haystack, $needle);
+        } else {
+            return stripos($haystack, $needle, $offset);
+        }
     }
 }
 
 /**
  * mb_stristr
  */
-function mb_stristr($haystack, $needle)
-{
-    return stristr($haystack, $needle);
+if (!function_exists('mb_stristr')) {
+
+    function mb_stristr($haystack, $needle)
+    {
+        return stristr($haystack, $needle);
+    }
 }
 
 /**
  * mb_strlen
  */
-function mb_strlen($str)
-{
-    return utf8_strlen($str);
+if (!function_exists('mb_strlen')) {
+
+    function mb_strlen($str)
+    {
+        return utf8_strlen($str);
+    }
 }
 
 /**
  * mb_strpos
  */
-function mb_strpos($haystack, $needle, $offset=null)
-{
-    if (null === $offset) {
-        return utf8_strpos($haystack, $needle);
-    } else {
-        return utf8_strpos($haystack, $needle, $offset);
+if (!function_exists('mb_strpos')) {
+
+    function mb_strpos($haystack, $needle, $offset = null)
+    {
+        if (null === $offset) {
+            return utf8_strpos($haystack, $needle);
+        } else {
+            return utf8_strpos($haystack, $needle, $offset);
+        }
     }
 }
-
 /**
  * mb_strrchr
  */
-function mb_strrchr($haystack, $needle)
-{
-    return utf8_strrchr($haystack, $needle);
+if (!function_exists('mb_strrchr')) {
+
+    function mb_strrchr($haystack, $needle)
+    {
+        return utf8_strrchr($haystack, $needle);
+    }
 }
 
 /**
  * mb_strrpos
  */
-function mb_strrpos($haystack, $needle)
-{
-    return utf8_strrpos($haystack, $needle);
+if (!function_exists('mb_strrpos')) {
+
+    function mb_strrpos($haystack, $needle)
+    {
+        return utf8_strrpos($haystack, $needle);
+    }
 }
 
 /**
  * mb_strstr
  */
-function mb_strstr($haystack, $needle)
-{
-    return utf8_strstr($haystack, $needle);
+if (!function_exists('mb_strstr')) {
+
+    function mb_strstr($haystack, $needle)
+    {
+        return utf8_strstr($haystack, $needle);
+    }
 }
 
 /**
  * mb_strtolower
  */
-function mb_strtolower($str)
-{
-    return utf8_strtolower($str);
+if (!function_exists('mb_strtolower')) {
+
+    function mb_strtolower($str)
+    {
+        return utf8_strtolower($str);
+    }
 }
 
 /**
  * mb_strtoupper
  */
-function mb_strtoupper($str)
-{
-    return utf8_strtoupper($str);
+if (!function_exists('mb_strtoupper')) {
+
+    function mb_strtoupper($str)
+    {
+        return utf8_strtoupper($str);
+    }
 }
 
 /**
  * mb_substr
  */
-function mb_substr($str, $start, $length=null)
-{
-    if (null === $length) {
-        return utf8_substr($str, $start);
-    } else {
-        return utf8_substr($str, $start, $length);
+if (!function_exists('mb_substr')) {
+
+    function mb_substr($str, $start, $length = null)
+    {
+        if (null === $length) {
+            return utf8_substr($str, $start);
+        } else {
+            return utf8_substr($str, $start, $length);
+        }
     }
 }
 
 /**
  * mb_substr_count
  */
-function mb_substr_count($haystack, $needle, $offset=null)
-{
-    if (null === $offset) {
-        return substr_count($haystack, $needle);
-    } else {
-        return substr_count($haystack, $needle, $offset);
+if (!function_exists('mb_substr_count')) {
+
+    function mb_substr_count($haystack, $needle, $offset = null)
+    {
+        if (null === $offset) {
+            return substr_count($haystack, $needle);
+        } else {
+            return substr_count($haystack, $needle, $offset);
+        }
     }
 }
