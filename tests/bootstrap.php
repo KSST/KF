12e21cbb5d04fe0fd37524ae9b56697d94b3946f
jakoch <jakoch@web.de>
2012-10-21 18:39:27 +0200

    moved autoload setup for tests into tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -21,7 +21,15 @@ if (!function_exists('_')) {
     }
 }
 
-include __DIR__ . '/_autoload.php';
+// Koch Framework Autoloader
+include_once __DIR__ . '/../framework/Koch/Autoload/Loader.php';
+$autoloader = new \Koch\Autoload\Loader();
+$autoloader->setClassMapFile(__DIR__ . '/autoloader.classmap.php'); // KF/tests/autoloader.classmap.php
+
+// Composer Autoloader
+if (is_file(__DIR__ . '/../vendor/autoload.php')) {
+    include_once __DIR__ . '/../vendor/autoload.php';
+}
 
 \Koch\Localization\Utf8::initialize();
 
