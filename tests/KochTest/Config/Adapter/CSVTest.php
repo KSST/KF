563ae27e8c7ab667ed1b284facc23cd19c8e62fa
Jens A. Koch <jakoch@web.de>
2012-11-04 19:15:57 +0100

    fixed test

--- tests/KochTest/Config/Adapter/CSVTest.php
+++ tests/KochTest/Config/Adapter/CSVTest.php
@@ -2,6 +2,9 @@
 namespace KochTest\Config\Adapter;
 
 use Koch\Config\Adapter\CSV;
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
+use org\bovigo\vfs\vfsStreamWrapper;
 
 class CSVTest extends \PHPUnit_Framework_TestCase
 {
@@ -17,37 +20,45 @@ class CSVTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->object = new CSV;
+
+        vfsStreamWrapper::register();
+        $this->configFileURL = vfsStream::url('root/config.csv');
+        $this->file = vfsStream::newFile('config.csv', 0777)->withContent($this->getConfigFileContent());
+        $this->root = new vfsStreamDirectory('root');
+        $this->root->addChild($this->file);
+        vfsStreamWrapper::setRoot($this->root);
     }
 
-    /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     */
     public function tearDown()
     {
+        unset($this->object);
     }
 
     /**
      * @covers Koch\Config\Adapter\CSV::readConfig
-     * @todo   Implement testReadConfig().
      */
     public function testReadConfig()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $array = $this->object->readConfig($this->configFileURL);
+        $this->assertEquals($array, $this->getConfigArray());
     }
 
     /**
      * @covers Koch\Config\Adapter\CSV::writeConfig
-     * @todo   Implement testWriteConfig().
      */
     public function testWriteConfig()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $result = $this->object->writeConfig($this->configFileURL, $this->getConfigArray());
+        $this->assertTrue($result);
+    }
+
+    public function getConfigFileContent()
+    {
+        return '1,2,3';
+    }
+
+    public function getConfigArray()
+    {
+        return array('1', '2', '3');
     }
 }
