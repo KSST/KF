d2e0e5559abb898ffa41792b1b52daa6a202a2b6
jakoch <jakoch@web.de>
2012-11-04 18:37:43 +0100

    namespace fixes

--- framework/Koch/Datatype/Conversion.php
+++ framework/Koch/Datatype/Conversion.php
@@ -52,7 +52,7 @@ class Conversion
         $xml->startElement('root');
 
         // add the $array data in between
-        $this->writeArray($xml, $array);
+        self::writeArray($xml, $array);
 
         // close with </root>
         $xml->endElement();
@@ -70,7 +70,7 @@ class Conversion
      * @param $xml XMLWriter Object
      * @param $array PHP Array
      */
-    public static function writeArray(XMLWriter $xml, array $array)
+    public static function writeArray(\XMLWriter $xml, array $array)
     {
         foreach ($array as $key => $value) {
             if (is_array($value)) {
