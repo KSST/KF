46ad573961bda2916ffb5838a039793a8c4d0f8b
jakoch <jakoch@web.de>
2012-11-05 17:52:38 +0100

    added comment

--- framework/Koch/Autoload/Loader.php
+++ framework/Koch/Autoload/Loader.php
@@ -339,6 +339,7 @@ class Loader
     public static function writeAutoloadingMapFile($array)
     {
         if (is_writable(self::$mapfile) === false) {
+            // create map file first
             self::readAutoloadingMapFile();
         }
 
