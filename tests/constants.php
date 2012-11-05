ee9634ed7498d326872e71a6de612115b3e28d27
Jens A. Koch <jakoch@web.de>
2012-11-05 16:18:44 +0100

    const for module path

--- tests/constants.php
+++ tests/constants.php
@@ -11,6 +11,6 @@ defined('REWRITE_ENGINE_ON')        || define('REWRITE_ENGINE_ON', false);
 
 defined('APPLICATION_PATH')         || define('APPLICATION_PATH', '/');
 defined('APPLICATION_CACHE_PATH')   || define('APPLICATION_CACHE_PATH', sys_get_temp_dir() . '/');
-defined('APPLICATION_MODULES_PATH') || define('APPLICATION_MODULES_PATH', '/');
+defined('APPLICATION_MODULES_PATH') || define('APPLICATION_MODULES_PATH', '/Modules/');
 
 //defined('VENDOR_PATH')            || define('VENDOR_PATH', __DIR__ . '../vendor/');
