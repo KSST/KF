7f84a569a78c7afb1b00ee5bebe989a7060f860d
jakoch <jakoch@web.de>
2012-11-05 18:21:43 +0100

    removed PHP_EOL

--- framework/Koch/Autoload/APCCompiler.php
+++ framework/Koch/Autoload/APCCompiler.php
@@ -46,30 +46,30 @@ class APCCompiler
 
     public function test()
     {
-        echo '<pre>' . PHP_EOL;
+        echo '<pre>' . "\n";
         if (function_exists('apc_compile_file')) {
 
             define('APC_CLEAR_CACHE', true);
             define('APC_COMPILE_RECURSIVELY', true);
             define('APC_COMPILE_DIR', '.');
 
-            echo 'APC Directory Compiler ' . gmdate('Y-m-d H:i:s') . PHP_EOL;
-            echo PHP_EOL . '-------------------------' . PHP_EOL;
+            echo 'APC Directory Compiler ' . gmdate('Y-m-d H:i:s') . "\n";
+            echo "\n" . '-------------------------' . "\n";
             if (APC_CLEAR_CACHE) {
-                echo (apc_clear_cache() ? 'Cache Cleaned' : 'Cache Not Cleaned') . PHP_EOL;
+                echo (apc_clear_cache() ? 'Cache Cleaned' : 'Cache Not Cleaned') . "\n";
                 var_dump(apc_cache_info());
-                echo PHP_EOL . '-------------------------' . PHP_EOL;
+                echo "\n" . '-------------------------' . "\n";
             }
-            echo 'Runtime Errors' . PHP_EOL;
+            echo 'Runtime Errors' . "\n";
             if (self::apcCompileDir(APC_COMPILE_DIR, APC_COMPILE_RECURSIVELY) === true) {
-                echo 'Cache Created' . PHP_EOL;
+                echo 'Cache Created' . "\n";
             } else {
-                echo 'Cache Not Created' . PHP_EOL;
+                echo 'Cache Not Created' . "\n";
             }
-            echo PHP_EOL . '-------------------------' . PHP_EOL;
+            echo "\n" . '-------------------------' . "\n";
             var_dump(apc_cache_info());
         } else {
-            echo 'APC is not present, nothing to do.' . PHP_EOL;
+            echo 'APC is not present, nothing to do.' . "\n";
             echo '</pre>';
         }
     }
