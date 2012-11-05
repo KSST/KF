6a41552d849117ee6202ab15998407e86313444f
jakoch <jakoch@web.de>
2012-11-05 19:19:02 +0100

    dropped test method, turned into static method, throws exception now when apc ext missing

--- framework/Koch/Autoload/APCCompiler.php
+++ framework/Koch/Autoload/APCCompiler.php
@@ -27,50 +27,24 @@ namespace Koch\Autoload;
 
 class APCCompiler
 {
-    public function compileDir($root, $recursively = true)
+    public static function compileDir($root, $recursively = true)
     {
+        if(extension_loaded('apc') === false) {
+            throw new \RuntimeException('APC Extensions not loaded.');
+        }
+
         $compiled = true;
 
         if (true === $recursively) {
-            foreach (glob($root . DIRECTORY_SEPARATOR . '*', GLOB_ONLYDIR) as $dir) {
+            foreach (glob($root . '/*', GLOB_ONLYDIR) as $dir) {
                 $compiled = $compiled && apc_compile_dir($dir, $recursively);
             }
         } else {
-            foreach (glob($root . DIRECTORY_SEPARATOR . '*.php') as $file) {
+            foreach (glob($root . '/*.php') as $file) {
                 $compiled = $compiled && apc_compile_file($file);
             }
         }
 
         return $compiled;
-    }
-
-    public function test()
-    {
-        echo '<pre>' . "\n";
-        if (function_exists('apc_compile_file')) {
-
-            define('APC_CLEAR_CACHE', true);
-            define('APC_COMPILE_RECURSIVELY', true);
-            define('APC_COMPILE_DIR', '.');
-
-            echo 'APC Directory Compiler ' . gmdate('Y-m-d H:i:s') . "\n";
-            echo "\n" . '-------------------------' . "\n";
-            if (APC_CLEAR_CACHE) {
-                echo (apc_clear_cache() ? 'Cache Cleaned' : 'Cache Not Cleaned') . "\n";
-                var_dump(apc_cache_info());
-                echo "\n" . '-------------------------' . "\n";
-            }
-            echo 'Runtime Errors' . "\n";
-            if (self::apcCompileDir(APC_COMPILE_DIR, APC_COMPILE_RECURSIVELY) === true) {
-                echo 'Cache Created' . "\n";
-            } else {
-                echo 'Cache Not Created' . "\n";
-            }
-            echo "\n" . '-------------------------' . "\n";
-            var_dump(apc_cache_info());
-        } else {
-            echo 'APC is not present, nothing to do.' . "\n";
-            echo '</pre>';
-        }
     }
 }
