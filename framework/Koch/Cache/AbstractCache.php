4b3a43084ae021d98c9c787ee49c9c1d4e7059db
Jens A. Koch <jakoch@web.de>
2012-11-21 23:08:38 +0100

    added method applyPrefix()

--- framework/Koch/Cache/AbstractCache.php
+++ framework/Koch/Cache/AbstractCache.php
@@ -20,9 +20,9 @@ abstract class AbstractCache
     /**
      * Prefix for the cache key.
      *
-     * @var mixed Defaults to 'cs'.
+     * @var mixed Defaults to 'kf_'.
      */
-    protected $prefix = 'cs';
+    protected $prefix = 'kf_';
 
     /**
      * Set Prefix for the cache key.
@@ -32,7 +32,7 @@ abstract class AbstractCache
      */
     public function setPrefix($prefix)
     {
-        if (empty($prefix)) {
+        if (empty($prefix) === true) {
             throw new \InvalidArgumentException('Prefix must not be empty.');
         }
 
@@ -47,5 +47,16 @@ abstract class AbstractCache
     public function getPrefix()
     {
         return $this->prefix;
+    }
+
+    /**
+     * Prepends key with prefix.
+     *
+     * @param string $key Cache Key.
+     * @return string Prefixed Cache Key.
+     */
+    public function applyPrefix($key)
+    {
+        return $this->prefix . $key;
     }
 }
