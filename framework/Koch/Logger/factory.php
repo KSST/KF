871d11d95d1dd8a01e9ca7cdd58b1ed9d060f4e4
Jens A. Koch <jakoch@web.de>
2012-11-22 17:35:38 +0100

    added logger factory

---
+++ framework/Koch/Logger/factory.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Koch Framework
+ * Jens A. Koch © 2005 - onwards
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Koch\Logger;
+
+/**
+ * A Factory for Logger Adapters.
+ */
+class Factory
+{
+    public function getAdapter($adapter)
+    {
+        $classname = ucfirst($adapter);
+        $file = __DIR__ . '/Adapter/'. $classname;
+        $class = '\Koch\Logger\Adapter\\' . $classname;
+        if (false === class_exists($class, false)) {
+            include $file;
+        }
+        return new $class;
+    }
+}
