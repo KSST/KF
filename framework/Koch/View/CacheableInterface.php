e4df5d2c972a00da1c77dd7d8dc8bc3945c69c1a
jakoch <jakoch@web.de>
2012-11-30 16:50:56 +0100

    header, semicolons

--- framework/Koch/View/CacheableInterface.php
+++ framework/Koch/View/CacheableInterface.php
@@ -1,10 +1,36 @@
+<?php
+
+/**
+ * Koch Framework
+ * Jens-André Koch © 2005 - onwards
+ *
+ * This file is part of "Koch Framework".
+ *
+ * License: GNU/GPL v2 or any later version, see LICENSE file.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace Koch\View;
 
 interface CacheableInterface {
 
     /**
      * Enables / disables the caching of templates.
      */
-    public function activateCaching($bool)
+    public function activateCaching($bool);
 
     /**
      * Checks if a template is cached.
@@ -14,10 +40,10 @@ interface CacheableInterface {
      * @param  mixed   $compile_id compile id to be used with this template
      * @return boolean Returns true in case the template is cached, false otherwise.
      */
-    public function isCached($template, $cache_id = null, $compile_id = null)
+    public function isCached($template, $cache_id = null, $compile_id = null);
 
     /**
      * Reset the Cache of the Renderer
      */
-    public function clearCache($template_name, $cache_id = null, $compile_id = null, $exp_time = null, $type = null)
+    public function clearCache($template_name, $cache_id = null, $compile_id = null, $exp_time = null, $type = null);
 }
