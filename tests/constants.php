cd639642246688f55304a0c25ac4470e2c8d327e
jakoch <jakoch@web.de>
2012-11-30 19:38:46 +0100

    added apc constant

--- tests/constants.php
+++ tests/constants.php
@@ -9,8 +9,11 @@ defined('WWW_ROOT')                 || define('WWW_ROOT', 'http://');           
 defined('WWW_ROOT_THEMES_CORE')     || define('WWW_ROOT_THEMES_CORE', 'http://');   // http://application/themes/core
 defined('REWRITE_ENGINE_ON')        || define('REWRITE_ENGINE_ON', false);
 
+defined('APPLICATION_NAME')         || define('APPLICATION_NAME', 'MYAPPLICATION');
 defined('APPLICATION_PATH')         || define('APPLICATION_PATH', '/');
 defined('APPLICATION_CACHE_PATH')   || define('APPLICATION_CACHE_PATH', sys_get_temp_dir() . '/');
 defined('APPLICATION_MODULES_PATH') || define('APPLICATION_MODULES_PATH', '/Modules/');
 
-//defined('VENDOR_PATH')            || define('VENDOR_PATH', __DIR__ . '../vendor/');
+defined('VENDOR_PATH')              || define('VENDOR_PATH', __DIR__ . '/../vendor/');
+
+defined('APC')                      || define('APC', extension_loaded('apc'));
