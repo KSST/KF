6da47050381c011331540e20a816a87ab54a1d7b
Jens-Andre Koch <jakoch@web.de>
2012-12-02 16:03:54 +0100

    added some response tests

--- tests/KochTest/Http/HttpResponseTest.php
+++ tests/KochTest/Http/HttpResponseTest.php
@@ -33,4 +33,68 @@ class HttpResponseTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertEquals(200, HttpResponse::getStatusCode());
     }
+
+    /**
+     * @covers HttpResponse::setStatusCode
+     * @covers HttpResponse::getStatusCode
+     */
+    public function testSetAndGetStatusCode()
+    {
+        $code = '200';
+        HttpResponse::setStatusCode($code);
+        $this->assertEquals($code, HttpResponse::getStatusCode());
+    }
+
+    public function testGetStatusCodeDescription()
+    {
+        $code = '200';
+        $this->assertEquals('OK', HttpResponse::getStatusCodeDescription($code));
+    }
+
+    /**
+     * @covers HttpResponse::setContent
+     * @covers HttpResponse::getContent
+     */
+    public function testSetContent()
+    {
+        $content = 'Some Content. This is the response body.';
+        HttpResponse::setContent($content);
+        $this->assertEquals($content, HttpResponse::getContent());
+
+        // append content
+        $content2 = 'Some additional content to test appending.';
+        HttpResponse::setContent($content2);
+        $this->assertEquals($content . $content2, HttpResponse::getContent());
+
+        // replace content
+        $content3 = ' This new Content replaces the old content.'
+        HttpResponse::setContent($content3, true);
+        $this->assertEquals($content3, HttpResponse::getContent());
+    }
+
+    /**
+     * @covers HttpResponse::setContentType
+     * @covers HttpResponse::getContentType
+     */
+    public function testSetAndGetContentType()
+    {
+        // default type
+        $this->assertEquals('html', HttpResponse::getContentType());
+
+        $type = 'xml';
+        HttpResponse::setContentType('xml');
+        $this->assertEquals($type, HttpResponse::getContentType());
+    }
+
+    /**
+     * @covers HttpResponse::setContentType
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage Specified type not valid. Use: html, txt, xml or json.
+     */
+    public function testSetContentType_throws()
+    {
+        HttpResponse::setContentType('SomeInvalidType');
+    }
+
+
 }
