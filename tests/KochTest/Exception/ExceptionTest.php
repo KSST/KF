9292115db596f920e96941bb43b1d0894a0f44f6
jakoch <jakoch@web.de>
2012-12-23 17:54:18 +0100

    added Exception test

---
+++ tests/KochTest/Exception/ExceptionTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace KochTest\Exception;
+
+use Koch\Exception\Exception;
+
+class ExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Exception
+     */
+    protected $exception;
+
+    public function setUp()
+    {
+        try {
+            $message = 'ExceptionMessage';
+            $code = 666;
+            $previous = null;
+            throw new Exception($message, $code, $previous);
+        } catch (Exception $e) {
+            $this->exception = $e;
+        }
+    }
+
+    public function tearDown()
+    {
+        unset($this->exception);
+        parent::tearDown();
+    }
+
+    public function testException()
+    {
+        $this->assertEquals('ExceptionMessage', $this->exception->getMessage());
+        $this->assertEquals(666, $this->exception->getCode());
+    }
+}
