2df53628e76319143bb3d74f1ccfeac242af0ca1
jakoch <jakoch@web.de>
2012-12-31 00:11:07 +0100

    cli color tests

--- tests/KochTest/Console/ColorsTest.php
+++ tests/KochTest/Console/ColorsTest.php
@@ -27,6 +27,12 @@ class ColorsTest extends \PHPUnit_Framework_TestCase
     public function testUnicodeSymbol()
     {
         $this->assertEquals('\342\234\227', Colors::unicodeSymbol('x'));
+
+        $this->assertEquals('\033[0;31m\342\234\227\033[0m', Colors::unicodeSymbol('x', 'red'));
+
+        $this->assertEquals('\342\234\223', Colors::unicodeSymbol('check', 'green'));
+
+        $this->assertEquals('\342\234\227', Colors::unicodeSymbol('x', array('green', 'black')));
     }
 
     /**
@@ -35,8 +41,19 @@ class ColorsTest extends \PHPUnit_Framework_TestCase
     public function testWrite()
     {
         $out = Colors::write('On a dark desert highway', 'red');
+        echo $out;
+        $this->assertEquals('\033[0;31mOn a dark desert highway\033[0m', $out);
 
-        $this->assertEquals('', $out);
+        $out = Colors::write('On a dark desert highway', 'green', 'black');
+        echo $out;
+
+        $out = Colors::write('On a dark desert highway', 'green', 'black', 'bold');
+        echo $out;
+
+        $out = Colors::write('On a dark desert highway', array('green', 'black', 'bold', 'underscore'));
+        echo $out;
+
+        $this->assertEquals("\033[32;40;1;4mOn a dark desert highway\033[0m", $out);
     }
 
     /**
@@ -46,6 +63,8 @@ class ColorsTest extends \PHPUnit_Framework_TestCase
     {
         $out = Colors::colorize('On a dark desert highway', 'dark desert', 'red');
 
-        $this->assertEquals('', $out);
+        echo $out;
+
+        $this->assertEquals('On a \033[0;31mdark desert\033[0m highway', $out);
     }
 }
