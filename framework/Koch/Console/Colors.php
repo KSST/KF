89e94549fdeade1de171fc92372dbe69e7cd201d
Jens-Andre Koch <jakoch@web.de>
2012-12-31 04:14:48 +0100

    varname fix, expected content fix

--- framework/Koch/Console/Colors.php
+++ framework/Koch/Console/Colors.php
@@ -104,7 +104,7 @@ class Colors
         return self::write($symbol, $options);
     }
 
-    public static function write($string, $foreground = null, $background = null, $modifiers = null)
+    public static function write($text, $foreground = null, $background = null, $modifiers = null)
     {
         if (is_array($foreground)) {
              $options = self::options($foreground);
