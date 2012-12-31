bb55d986a3a732e99a2dbab6ac3055a2180622eb
Jens-Andre Koch <jakoch@web.de>
2012-12-31 04:01:25 +0100

    check for var

--- framework/Koch/Console/Colors.php
+++ framework/Koch/Console/Colors.php
@@ -124,9 +124,11 @@ class Colors
             $codes[] = self::$background[$background];
         }
 
-        foreach($modifiers as $modifier) {
-             if (isset(self::$modifier[$modifier])) {
-                $codes[] = self::$modifier[$modifier];
+        if(null !== $modifiers) {
+            foreach($modifiers as $modifier) {
+                 if (isset(self::$modifier[$modifier])) {
+                    $codes[] = self::$modifier[$modifier];
+                }
             }
         }
 
