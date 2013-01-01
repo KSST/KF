6c53fcf9b654ce57fe15ea2bc7dbcb1f60b0c52b
Jens A. Koch <jakoch@web.de>
2013-01-01 14:05:29 +0100

    QA

--- framework/Koch/Doctrine/TablePrefix.php
+++ framework/Koch/Doctrine/TablePrefix.php
@@ -14,15 +14,32 @@ namespace Koch\Doctrine;
 
 use \Doctrine\ORM\Event\LoadClassMetadataEventArgs;
 
-class TablePrefix
+class TablePrefix implements \Doctrine\Common\EventSubscriber
 {
+    /**
+     * @var string
+     */
     protected $prefix = '';
 
+    /**
+     * @param $prefix
+     */
     public function __construct($prefix)
     {
         $this->prefix = (string) $prefix;
     }
 
+    /**
+     * @return array
+     */
+    public function getSubscribedEvents()
+    {
+        return array('loadClassMetadata');
+    }
+
+    /**
+     * @param \Doctrine\ORM\Event\LoadClassMetadataEventArgs $eventArgs
+     */
     public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs)
     {
         $classMetadata = $eventArgs->getClassMetadata();
@@ -30,7 +47,8 @@ class TablePrefix
         $classMetadata->setTableName($this->prefix . $classMetadata->getTableName());
 
         foreach ($classMetadata->getAssociationMappings() as $fieldName => $mapping) {
-            if ($mapping['type'] == \Doctrine\ORM\Mapping\ClassMetadataInfo::MANY_TO_MANY) {
+            if ($mapping['type'] == \Doctrine\ORM\Mapping\ClassMetadataInfo::MANY_TO_MANY
+                && isset($classMetadata->associationMappings[$fieldName]['joinTable']['name'])) {
                 $mappedTableName = $classMetadata->associationMappings[$fieldName]['joinTable']['name'];
                 $classMetadata->associationMappings[$fieldName]['joinTable']['name'] = $this->prefix . $mappedTableName;
             }
