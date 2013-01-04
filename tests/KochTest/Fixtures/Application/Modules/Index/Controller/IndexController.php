297d8d1e916617a1c9d1793efb8202ac68ccaab3
Jens A. Koch <jakoch@web.de>
2013-01-04 01:36:59 +0100

    renamed folder

--- tests/KochTest/Fixtures/Application/Modules/Index/Controller/IndexController.php
+++ tests/KochTest/Fixtures/Application/Modules/Index/Controller/IndexController.php
@@ -2,12 +2,12 @@
 /**
  * This explains your WTF!? when seeing the namespace...
  *
- * Application Namespace = KochTest\Router\Fixtures\Application
+ * Application Namespace = KochTest\Fixtures\APP\
  * Module Namespace      = \Modules
  * Module                = \Index
  * Controller            = \Controller\IndexController
  */
-namespace KochTest\Router\Fixtures\Application\Modules\Index\Controller;
+namespace KochTest\Fixtures\APP\Modules\Index\Controller;
 
 use Koch\Module\ModuleInterface;
 
