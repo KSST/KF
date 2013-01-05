51e7f51cb65512c0c20222a01ab684d41a5eed4e
jakoch <jakoch@web.de>
2013-01-05 23:53:34 +0100

    exit, if composers autoload was not found

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -29,6 +29,9 @@ $autoloader->setClassMapFile(__DIR__ . '/autoloader.classmap.php'); // KF/tests/
 // Composer Autoloader
 if (is_file(__DIR__ . '/../vendor/autoload.php')) {
     include_once __DIR__ . '/../vendor/autoload.php';
+} else {
+    echo 'Could not find "vendor/autoload.php". Did you forget to run "composer install --dev"?' . PHP_EOL;
+    exit(1);
 }
 
 \Koch\Localization\Utf8::initialize();
