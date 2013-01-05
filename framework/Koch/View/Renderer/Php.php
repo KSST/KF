d9f3428e56d217ed892afb0b7900026b56969aac
jakoch <jakoch@web.de>
2013-01-05 02:27:38 +0100

    fixed render method

--- framework/Koch/View/Renderer/Php.php
+++ framework/Koch/View/Renderer/Php.php
@@ -120,7 +120,7 @@ class Php extends AbstractRenderer
      *
      * @return string HTML Representation of Template with Vars
      */
-    public function render($template, $viewdata)
+    public function render($template, $viewdata = null)
     {
         $this->assign($viewdata);
 
