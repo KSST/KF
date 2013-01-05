0b018612a5da86ed6678679c12ae326441fb7d3d
Jens A. Koch <jakoch@web.de>
2013-01-05 21:27:21 +0100

    added form decorator "noerror"

---
+++ framework/Koch/Form/Decorators/Form/NoError.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Koch Framework
+ * Jens A. Koch © 2005 - onwards
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Koch\Form\Decorators\Form;
+
+use Koch\Form\FormDecorator;
+
+class NoError extends FormDecorator
+{
+    /**
+     * @var string Name of this decorator
+     */
+    public $name = 'noerror';
+
+    public function render($html_form_content)
+    {
+        return $html_form_content;
+    }
+}
