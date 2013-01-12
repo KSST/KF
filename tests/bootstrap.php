f39c2b9895fe508654859a87779fbf3b0053681f
jakoch <jakoch@web.de>
2013-01-12 01:02:25 +0100

    don't let composer be on first position in the autoloader stack, added tests for framework constants

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -21,11 +21,6 @@ if (!function_exists('_')) {
     }
 }
 
-// Koch Framework Autoloader
-include_once __DIR__ . '/../framework/Koch/Autoload/Loader.php';
-$autoloader = new \Koch\Autoload\Loader();
-$autoloader->setClassMapFile(__DIR__ . '/autoloader.classmap.php'); // KF/tests/autoloader.classmap.php
-
 // Composer Autoloader
 if (is_file(__DIR__ . '/../vendor/autoload.php')) {
     include_once __DIR__ . '/../vendor/autoload.php';
@@ -33,6 +28,12 @@ if (is_file(__DIR__ . '/../vendor/autoload.php')) {
     echo 'Could not find "vendor/autoload.php". Did you forget to run "composer install --dev"?' . PHP_EOL;
     exit(1);
 }
+
+// Koch Framework Autoloader
+// Note: We are on top of the autoloader stack and not composer's classloader.
+include_once __DIR__ . '/../framework/Koch/Autoload/Loader.php';
+$autoloader = new \Koch\Autoload\Loader();
+$autoloader->setClassMapFile(__DIR__ . '/autoloader.classmap.php'); // KF/tests/autoloader.classmap.php
 
 \Koch\Localization\Utf8::initialize();
 
