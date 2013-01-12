d3b863e38c4f51ba81840673e8f869377cbfa4b1
jakoch <jakoch@web.de>
2013-01-12 23:24:44 +0100

    fixed failed to open stream: "org\bovigo\vfs\vfsStreamWrapper::stream_open" call failed

--- tests/KochTest/Autoload/LoaderTest.php
+++ tests/KochTest/Autoload/LoaderTest.php
@@ -199,7 +199,7 @@ class LoaderTest extends \PHPUnit_Framework_TestCase
     public function testMethod_readAutoloadingMapApc()
     {
         if (!extension_loaded('apc')) {
-            $this->markTestSkipped('APC extensions required.');
+            $this->markTestSkipped(' This test requires the PHP extension "apc".');
         }
 
         $this->assertSame(apc_fetch('CLANSUITE_CLASSMAP'), Loader::readAutoloadingMapApc());
