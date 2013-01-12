1a3632346a2a3a13e817f185dee8dda0a15f12f0
jakoch <jakoch@web.de>
2013-01-12 15:43:00 +0100

    added missing ns

--- framework/Koch/Files/Download.php
+++ framework/Koch/Files/Download.php
@@ -114,7 +114,7 @@ class Download
             }
             // returns the mime type and mime encoding as defined by RFC 2045
             // @see http://php.net/manual/de/fileinfo.constants.php
-            return finfo::file($file, FILEINFO_MIME);
+            return \finfo::file($file, FILEINFO_MIME);
         } elseif (array_key_exists($extension, $mime_types)) {
             return $mime_types[$extension];
         } else {
