ea3aa8e283f9b0ebfea91c09d7fbdd82bfe90f99
Jens A. Koch <jakoch@web.de>
2013-02-24 18:52:23 +0100

    added pagination adapter doctrine test case

--- framework/Koch/PhpUnit/DoctrineTestCase.php
+++ framework/Koch/PhpUnit/DoctrineTestCase.php
@@ -36,7 +36,7 @@ class DoctrineTestCase extends TestCase
         parent::setUp();
 
         if (!extension_loaded('pdo_sqlite') or !extension_loaded('sqlite')) {
-            $this->markTestSkipped('This test requires the php extension "pdo_sqlite" or "sqlite".');
+            $this->markTestSkipped('This test requires the PHP extension "pdo_sqlite" or "sqlite".');
         }
 
         $driver = AnnotationDriver::create(__DIR__ . '/KochTest/Fixtures/Doctrine/Entity');
--- tests/KochTest/Pagination/Adapter/DoctrineTest.php
+++ tests/KochTest/Pagination/Adapter/DoctrineTest.php
@@ -1,19 +1,90 @@
 <?php
 namespace KochTest\Pagination;
 
-class DoctrineTest extends \PHPUnit_Framework_TestCase
+use \Koch\Pagination\Adapter\Doctrine;
+use \Koch\PHPUnit\DoctrineTestCase;
+
+// doctrine/data-fixtures
+use Doctrine\Common\DataFixtures\Loader;
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+
+class DoctrineTest extends DoctrineTestCase
 {
     /**
-     * @var Doctrine
+     * @var \Koch\Pagination\Adapter\DoctrineCollection
      */
     protected $object;
 
-    protected function setUp()
+    public $collection;
+
+    public function setUp()
     {
-        $this->object = new Doctrine;
+        parent::setUp();
+
+        // Setup Doctrine2 fixtures
+        $loader = new Loader();
+        $loader->loadFromDirectory(__DIR__ . '/../../../KochTest/Fixtures/Pagination');
+
+        // execute fixtures
+        $fixtures = $loader->getFixtures();
+        $em = $this->getEntityManager();
+        $purger = new ORMPurger();
+        $executor = new ORMExecutor($em, $purger);
+        $executor->execute($fixtures);
+
+        $this->object = new Doctrine($this->collection);
     }
 
     protected function tearDown()
     {
+        unset($this->object);
+    }
+
+    public function testGetCollection()
+    {
+        $this->assertSame($this->collection, $this->object->getCollection());
+    }
+
+    public function testgetTotalNumberOfResults()
+    {
+        $this->collection
+            ->expects($this->once())
+            ->method('count')
+            ->will($this->returnValue(120));
+
+        $this->assertSame(120, $this->object->getTotalNumberOfResults());
+    }
+
+    /**
+     * @dataProvider getResultsProvider
+     */
+    public function testGetResults($offset, $length)
+    {
+        $this->collection
+            ->expects($this->once())
+            ->method('slice')
+            ->with($offset, $length)
+            ->will($this->returnValue($all = array(new \DateTime(), new \DateTime())));
+
+        $this->assertSame($all, $this->object->getSlice($offset, $length));
+    }
+
+    public function getResultsProvider()
+    {
+        return array(
+            array(3, 8),
+            array(3, 6),
+        );
+    }
+
+    public function testGetArray()
+    {
+        $this->collection
+            ->expects($this->once())
+            ->method('toArray')
+            ->will($this->returnValue(array('a', 'b')));
+
+        $this->assertEquals(array('a', 'b'), $this->object->getArray());
     }
 }
