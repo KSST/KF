d2177070017bde59da9addeb41fea7e1df2983f7
jakoch <jakoch@web.de>
2013-02-28 16:45:15 +0100

    NS fix

--- tests/KochTest/PHPUnit/DoctrineTestCaseTest.php
+++ tests/KochTest/PHPUnit/DoctrineTestCaseTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace KochTest\Debug;
+namespace KochTest\PHPUnit;
 
 use Koch\PHPUnit\DoctrineTestCase;
 
@@ -11,19 +11,11 @@ class DoctrineTestCaseTest extends \PHPUnit_Framework_TestCase
      */
     protected $object;
 
-    /**
-     * Sets up the fixture, for example, opens a network connection.
-     * This method is called before a test is executed.
-     */
     public function setUp()
     {
         $this->object = new DoctrineTestCase;
     }
 
-    /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     */
     public function tearDown()
     {
         unset($this->object);
