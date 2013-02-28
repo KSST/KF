6e2de465a32d8a5f3c62f4051447b8e3022b7e88
Jens A. Koch <jakoch@web.de>
2013-02-28 12:45:23 +0100

    enable pdo_sqlite on travis

--- bin/travis-setup.php
+++ bin/travis-setup.php
@@ -26,6 +26,9 @@ if(version_compare(PHP_VERSION, '5.4.0', '<'))
     $phpEnv->iniSet('short_open_tag=On');
 }
 
+// enable sqlite (for database tests)
+$phpEnv->iniSet('extension=pdo_sqlite.so');
+
 class PhpEnvironment
 {
     /**
