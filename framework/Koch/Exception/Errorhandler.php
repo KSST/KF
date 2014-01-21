4e9f7eea1867bfced4115be1ed6fbcffe4e815dd
Jens A. Koch <jakoch@web.de>
2014-01-21 15:22:02 +0100

    phpdoc fixes

--- framework/Koch/Exception/Errorhandler.php
+++ framework/Koch/Exception/Errorhandler.php
@@ -42,10 +42,10 @@ class Errorhandler
     /**
      * Handle Error as ErrorException
      *
-     * @param  type            $errnum
-     * @param  type            $message
-     * @param  type            $filename
-     * @param  type            $lineno
+     * @param  int $errnum     contains the error as integer.
+     * @param  string $message     contains the error string.
+     * @param  string $filename    contains the filename with occuring error.
+     * @param  int $lineno     contains the line of error.
      * @throws \ErrorException
      */
     public function handleErrorAsErrorException($errnum, $message, $filename, $lineno)
@@ -64,7 +64,7 @@ class Errorhandler
      * @link http://www.php.net/manual/de/errorfunc.constants.php
      *
      * @param int $errnum     contains the error as integer.
-     * @param string  $errstr     contains error string info.
+     * @param string  $errstr     contains the error string.
      * @param string  $errfile    contains the filename with occuring error.
      * @param string  $errline    contains the line of error.
      * @param string  $errcontext (optional) array with variables from error context.
