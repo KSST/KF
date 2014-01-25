48794f176ddf6c25ba63ed6bf89205960f9417f5
Jens A. Koch <jakoch@web.de>
2014-01-26 00:02:14 +0100

    added acl stub

---
+++ framework/Koch/Permissions/Acl.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Koch Framework
+ * Jens A. Koch © 2005 - onwards
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Koch\Permissions;
+
+/**
+ * ACL
+ */
+class Acl
+{
+    public function addRole($roleGroup, $role)
+    {
+
+    }
+
+    public function addResource($resourceGroup, $resource)
+    {
+
+    }
+
+    public function ruleAllow($role, $action, $resource)
+    {
+
+    }
+
+    public function ruleDeny($role, $action, $resource)
+    {
+
+    }
+}
