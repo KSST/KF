d5f900163b6774863b1acd1911a4d8e84c406683
jakoch <jakoch@web.de>
2014-04-13 19:50:05 +0200

    removed UUID generation function, new implementation of deleteDir(()

--- framework/Koch/Functions/Functions.php
+++ framework/Koch/Functions/Functions.php
@@ -129,59 +129,6 @@ class Functions
     }
 
     /**
-     * @brief Generates a Universally Unique IDentifier, version 4.
-     *
-     * This function generates a truly random UUID.
-     *
-     * @author: sean at seancolombo dot com; 06.01.2009; http://www.php.net/uniqid
-     * @see http://tools.ietf.org/html/rfc4122#section-4.4
-     * @see http://en.wikipedia.org/wiki/UUID
-     * @return string A UUID, made up of 32 hex digits and 4 hyphens.
-     */
-    public static function generateSecureUUID()
-    {
-
-        $pr_bits = null;
-        $fp = fopen('/dev/urandom', 'rb');
-        if ($fp !== false) {
-            $pr_bits .= @ fread($fp, 16);
-            fclose($fp);
-        } else {
-            // If /dev/urandom isn't available (eg: in non-unix systems), use mt_rand().
-            $pr_bits = '';
-            for ($cnt = 0; $cnt < 16; $cnt++) {
-                $pr_bits .= chr(mt_rand(0, 255));
-            }
-        }
-
-        $time_low = bin2hex(substr($pr_bits, 0, 4));
-        $time_mid = bin2hex(substr($pr_bits, 4, 2));
-        $time_hi_and_version = bin2hex(substr($pr_bits, 6, 2));
-        $clock_seq_hi_and_reserved = bin2hex(substr($pr_bits, 8, 2));
-        $node = bin2hex(substr($pr_bits, 10, 6));
-
-        /**
-         * Set the four most significant bits (bits 12 through 15) of the
-         * time_hi_and_version field to the 4-bit version number from
-         * Section 4.1.3.
-         * @see http://tools.ietf.org/html/rfc4122#section-4.1.3
-         */
-        $time_hi_and_version = hexdec($time_hi_and_version);
-        $time_hi_and_version = $time_hi_and_version >> 4;
-        $time_hi_and_version = $time_hi_and_version | 0x4000;
-
-        /**
-         * Set the two most significant bits (bits 6 and 7) of the
-         * clock_seq_hi_and_reserved to zero and one, respectively.
-         */
-        $clock_seq_hi_and_reserved = hexdec($clock_seq_hi_and_reserved);
-        $clock_seq_hi_and_reserved = $clock_seq_hi_and_reserved >> 2;
-        $clock_seq_hi_and_reserved = $clock_seq_hi_and_reserved | 0x8000;
-
-        return sprintf('%08s-%04s-%04x-%04x-%012s', $time_low, $time_mid, $time_hi_and_version, $clock_seq_hi_and_reserved, $node);
-    }
-
-    /**
      * Calculates the size of a directory (recursiv)
      *
      * Hint: use return getsize() on the return value of this function transform it to a readable format.
@@ -747,52 +694,33 @@ class Functions
     }
 
     /**
-     * Delete a directory or just it's content recursively
+     * Recursively delete directory using PHP iterators.
      *
-     * @param $directory directory string Name / Path of the Directory to delete.
-     * @param $delete_dir_itself boolean Delete the directory itself. Default false.
+     * Uses a CHILD_FIRST RecursiveIteratorIterator to sort files
+     * before directories, creating a single non-recursive loop
+     * to delete files/directories in the correct order.
+     *
+     * @param  string $directory
+     * @param  bool   $delete_dir_itself
+     * @return bool
      */
-    public static function deleteDirContent($directory, $delete_dir_itself = false)
+    public function deleteDir($directory, $delete_dir_itself = false)
     {
-        if (substr($directory, -1) == '/') {
-            $directory = substr($directory, 0, -1);
+        $it = new RecursiveDirectoryIterator($directory, RecursiveDirectoryIterator::SKIP_DOTS);
+        $ri = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::CHILD_FIRST);
+
+        foreach ($ri as $file) {
+            if ($file->isDir()) {
+                rmdir($file->getPathname());
+            } else {
+                unlink($file->getPathname());
+            }
         }
 
-        if (!is_file($directory) or !is_dir($directory)) {
-            return false;
-        } elseif (!is_readable($directory)) {
-            return false;
-        } else {
-            // loop over all elements in that directory
-            $handle = opendir($directory);
-            while (false !== ( $item = readdir($handle))) {
-                if ($item != '.' and $item != '..') {
-                    // path of that element (dir/file)
-                    $path = $directory . '/' . $item;
-
-                    // delete dir
-                    if (is_dir($path) === true) {
-                        // remove the content and the directory itself
-                        self::delete_dir_content($path, true);
-                    } else { // delete file
-                       unlink($path);
-                    }
-                }
-            }
-            closedir($handle);
-
-            // remove that subdir
-            if ($delete_dir_itself === true) {
-                if (rmdir($directory) == false) {
-                    return false;
-                }
-            }
-
-            return true;
+        if ($delete_dir_itself === true) {
+            return rmdir($directory);
         }
-
-        return false;
-    }
+     }
 
     public static function shortenString($string, $maxlength = 50, $append_string = '[...]')
     {
